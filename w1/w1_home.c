a7b30c1a8e09de64f35737a827b3536cefd86bb8
CodeRed23 <39540565+CodeRed23@users.noreply.github.com>
2019-09-21 10:07:49 -0400

    Add files via upload

---
+++ w1/w1_home.c
@@ -0,0 +1,9 @@
+/*Nicholas DeFranco ndefranco@myseneca.ca 106732183 */
+#include <stdio.h>
+int main(void)
+{
+	printf("********************************\n");
+	printf("*** Welcome to C Programming ***\n");
+	printf("********************************\n");
+	return 0;
+}
---
+++ w1/w1_lab.c
@@ -0,0 +1,7 @@
+/*Nicholas DeFranco ndefranco@myseneca.ca 106732183 */
+#include <stdio.h>
+int main(void)
+{
+	printf("*** Welcome to C Programming ***\n");
+	return 0;
+}
